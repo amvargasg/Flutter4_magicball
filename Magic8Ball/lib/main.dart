4a8c6807107b917d43b37958bcbbad45bbb4a938
Angela Vargas <angela.v@ibm.com>
2022-02-23 00:16:44 -0500

    add files

---
+++ Magic8Ball/lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent.shade400,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Text('Ask me anything'),
+        ),
+        body: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int b = 1;
+
+  void shakeBall() {
+    setState(() {
+      b = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          shakeBall();
+        },
+        child: Image.asset('images/ball$b.png'),
+      ),
+    );
+  }
+}
